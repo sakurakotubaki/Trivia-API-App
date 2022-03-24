ce56d7eac0d0b03b453876cd25d8f4610f9ddd4a
Flutter_SakuSaku13:44 <gitonly543@gmail.com>
2022-03-24 23:24:23 +0900

    ボタンを2個作成

--- lib/pages/game_page.dart
+++ lib/pages/game_page.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 
 class GamePage extends StatelessWidget {
-  const GamePage({Key? key}) : super(key: key);
   double? _deviceHeight, _deviceWidth;
 
   @override
@@ -25,6 +24,59 @@ class GamePage extends StatelessWidget {
 
   // ゲームのUI関数
   Widget _gameUI() {
-    return Column();
+    return Column(
+      // テキストを中央よせせにする
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      mainAxisSize: MainAxisSize.max,
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: [
+        _questionText(),
+        Column(
+          children: [
+            _trueButton(),
+            SizedBox(height: _deviceHeight! * 0.01,),
+            _falseButton(),
+          ],
+        ),
+      ],
+    );
+  }
+
+  // 質問テキストの関数
+  Widget _questionText() {
+    return const Text(
+      "Test Question 1, Nothing Interrestiong",
+      style: TextStyle(
+        color: Colors.white,
+        fontSize: 25,
+        fontWeight: FontWeight.w400,
+      ),
+    );
+  }
+
+  Widget _trueButton() {
+    return MaterialButton(
+      onPressed: () {},
+      color: Colors.green,
+      minWidth: _deviceWidth! * 0.80,
+      height: _deviceHeight! * 0.10,
+      child: const Text(
+        "True",
+        style: TextStyle(color: Colors.white, fontSize: 25),
+      ),
+    );
+  }
+
+  Widget _falseButton() {
+    return MaterialButton(
+      onPressed: () {},
+      color: Colors.red,
+      minWidth: _deviceWidth! * 0.80,
+      height: _deviceHeight! * 0.10,
+      child: const Text(
+        "False",
+        style: TextStyle(color: Colors.white, fontSize: 25),
+      ),
+    );
   }
 }
