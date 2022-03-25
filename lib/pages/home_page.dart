dae52eef640909de1aa655ade3c3f820ae52f4db
Flutter_SakuSaku13:44 <gitonly543@gmail.com>
2022-03-25 21:32:54 +0900

    Navigator.push OK!

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:friviaa/pages/game_page.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -30,6 +31,7 @@ class _HomePageState extends State<HomePage> {
             children: [
               _appTitle(),
               _difficultySlider(),
+              _startGameButton(),
             ],
           ),
         ),
@@ -49,18 +51,16 @@ class _HomePageState extends State<HomePage> {
         Text(
           _difficultyTexts[_currentDifficultyLevel.toInt()],
           style: const TextStyle(
-            color: Colors.white,
-            fontSize: 20,
-            fontWeight: FontWeight.w500
-          ),
+              color: Colors.white, fontSize: 20, fontWeight: FontWeight.w500),
         ),
       ],
     );
   }
 
+  // スライダー関数
   Widget _difficultySlider() {
     return Slider(
-      label: "difficulty",
+        label: "difficulty",
         min: 0,
         max: 2,
         divisions: 2,
@@ -71,5 +71,24 @@ class _HomePageState extends State<HomePage> {
             _currentDifficultyLevel = _value;
           });
         });
+  }
+
+  // ゲームスタートする関数
+  Widget _startGameButton() {
+    return MaterialButton(
+      onPressed: () {
+        Navigator.push(context,
+            MaterialPageRoute(builder: (BuildContext _context) {
+          return GamePage(); // GamePageの画面へ遷移する
+        }));
+      },
+      color: Colors.blue,
+      minWidth: _deviceWidth! * 0.80, // デバイスの最大幅
+      height: _deviceHeight! * 0.10,
+      child: const Text(
+        "Start",
+        style: TextStyle(color: Colors.white, fontSize: 25),
+      ),
+    );
   }
 }
