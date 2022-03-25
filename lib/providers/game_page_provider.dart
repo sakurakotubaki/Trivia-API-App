32b8e061b2f842da23674608501cc5bfbb74dce1
Flutter_SakuSaku13:44 <gitonly543@gmail.com>
2022-03-25 19:20:51 +0900

    api get OK

--- lib/providers/game_page_provider.dart
+++ lib/providers/game_page_provider.dart
@@ -1,12 +1,26 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:dio/dio.dart';
 
 class GamePageProvider extends ChangeNotifier {
   final Dio _dio = Dio();
+  final int _maxQuestions = 10;
 
   BuildContext context;
   GamePageProvider({required this.context}) {
     _dio.options.baseUrl = 'https://opentdb.com/api.php';
-    print("Hello");
+    _getQuestionsFromAPI();
+  }
+  Future<void> _getQuestionsFromAPI() async {
+    var _response = await _dio.get(
+      '',
+      queryParameters: {'amount': 10, 'type': 'boolean', 'difficulty': 'easy'},
+    );
+    // データーを保存
+    var _data = jsonDecode(
+      _response.toString(),
+    );
+    print(_data);
   }
 }
